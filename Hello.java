e25b39eff9bc3d7b7a09c173dda19c543550f8a1
MahiLokesh <116069162+MahiLokesh@users.noreply.github.com>
2022-10-18 14:50:31 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package pk;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("HELLO WORLD");
+		
+	
+		System.out.println("I'm Lokesh");
+
+	}
+
+}
