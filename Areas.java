970c88e69d7d8e698eb0a46c17ffdab0c5fce2d5
MahiLokesh <116069162+MahiLokesh@users.noreply.github.com>
2022-10-18 17:01:32 +0530

    Add files via upload

---
+++ Areas.java
@@ -0,0 +1,33 @@
+package area;
+
+public class Areas {
+
+	public static void main(String[] args) {
+		float pi, r, area;
+		pi=3.14f;
+		r=5;
+		area= pi*r*r;
+		System.out.println("The area of circle is:" +area);
+		
+		float b,h, Triangle;
+		b=5;
+		h=6;
+		Triangle=(b*h)/2;
+		System.out.println("The area of triangle is:" +Triangle);
+		
+		int width, height, Rect;
+		width=4;
+		height=10;
+		Rect=width*height;
+		System.out.println("The area of rectangle is:" +Rect);
+		
+		int a, Circle;
+		a=5;
+		Circle=a*a;
+		System.out.println("The area of square is:" +Circle);
+		
+
+
+	}
+
+}
