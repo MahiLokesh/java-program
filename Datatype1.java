0d1bb87ce3ae992b58928872954279b4eb44afe1
MahiLokesh <116069162+MahiLokesh@users.noreply.github.com>
2022-10-18 15:40:04 +0530

    Add files via upload

---
+++ Datatype1.java
@@ -0,0 +1,35 @@
+package datatype;
+
+public class Datatype1 {
+
+	public static void main(String[] args) {
+		//declaration of variables
+	int intval;
+	float fval;
+	double dval;
+	char cval;
+	short sval;
+	byte bval;
+	long lval;
+	boolean la;
+	
+	//Assign the values
+	intval=468;
+	
+	System.out.println("Integer value=" +intval);
+	fval=468.5f;
+	System.out.println("Float value=" +fval);
+	
+	dval=468.9;
+	cval='A';
+	lval=9871;
+	la=true;
+	
+	System.out.println("Double value=" +dval);
+	System.out.println("Char value=" +cval);
+	System.out.println("Long value=" +lval);
+	System.out.println("Boolean value=" +la);
+
+	}
+
+}
