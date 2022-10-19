011434306ca3c0645a9382600f1c652bd00ed050
MahiLokesh <116069162+MahiLokesh@users.noreply.github.com>
2022-10-19 14:46:22 +0530

    Add files via upload

---
+++ Temporary.java
@@ -0,0 +1,23 @@
+package temp;
+
+public class Temporary {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=20;
+		b=40;
+		System.out.println("######      BEFORE SWAPPING     ####");
+		System.out.println("The value of a:" +a);
+		System.out.println("The value of b:" +b);
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("######      AFTER SWAPPING     ####");
+		System.out.println("The value of a:" +a);
+		System.out.println("The value of b:" +b);
+		
+
+	}
+
+}
